19f00cc94f2489f60e835eeedf5f1dcb63392e9b
Andrey Ushakov <ushakov.andrey257686@gmail.com>
2023-01-04 14:31:26 +0500

    added auth modal

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
 const cardButton = document.querySelector('#card-button');
-const modal = document.querySelector('.modal');
-const closeButton = document.querySelector('.close');
+const modalCard = document.querySelector('.modal-card');
+const closeCardButton = document.querySelector('.close-card');
 
-cardButton.addEventListener('click', toggleModal);
-closeButton.addEventListener('click', toggleModal);
+cardButton.addEventListener('click', toggleModalCard);
+closeCardButton.addEventListener('click', toggleModalCard);
 
-function toggleModal () {
-  modal.classList.toggle('is-open');
+function toggleModalCard () {
+  modalCard.classList.toggle('is-open');
 }
+
+// ------------------- Авторищация ---------------------------
+
+const authButton = document.querySelector('#auth-button');
+const modalAuth = document.querySelector('.modal-auth');
+const closeAuthButton = document.querySelector('.close-auth');
+
+authButton.addEventListener('click', toggleModalAuth);
+closeAuthButton.addEventListener('click', toggleModalAuth);
+
+function toggleModalAuth () {
+  modalAuth.classList.toggle('is-open');
+}
+
+
 
 new WOW().init();
