c7c7497a4546298409a06898622e73f57a42e510
Andrey Ushakov <ushakov.andrey257686@gmail.com>
2023-01-09 20:09:05 +0500

    modified index.html

--- js/menu.js
+++ js/menu.js
@@ -4,7 +4,7 @@ const renderItems = (data) => {
   console.log(data);
 }
 
-fetch(`./db/${restaurant}.json`)
+fetch(`https://test-ef01d-default-rtdb.firebaseio.com/db/${restaurant}.json`)
   .then((response) => response.json())
   .then((data) => {
     renderItems(data);
